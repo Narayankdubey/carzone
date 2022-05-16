f8b8ab3765bbe1673f3d7758c961d98aff5d6b31
Narayan Dubey <narayan.d@valuebound.com>
2022-05-16 22:12:49 +0530

    added search,filter and sort ui

--- src/Components/user/Buy/Index.js
+++ src/Components/user/Buy/Index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Form, FormControl } from "react-bootstrap";
 import { SortAlphaDown, Funnel } from "react-bootstrap-icons";
 
 import "./style.css";
@@ -10,6 +10,30 @@ const Buy = () => {
       className="sell-container"
       style={{ display: "flex", flexDirection: "column" }}
     >
+      <div className="search-filter-container">
+        <Form className="d-flex search-container" >
+          <FormControl
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+        </Form>
+        <div className="sort-filter-container">
+          <Form.Select aria-label="Default select example">
+            <option>Sort By</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </Form.Select>
+          <Form.Select aria-label="Default select example">
+            <option>Filter</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </Form.Select>
+        </div>
+      </div>
       <div
         className="sell-product-container"
         style={{
